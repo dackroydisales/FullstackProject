926f9fdb13807beee95c4c78914278035eb4951b
Daniel Ackroyd_Isales <dackroydisales@gmail.com>
2020-10-12 17:52:53 -0700

    add Users migration, and add root routes and React entry file

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 module.exports = {
     context: __dirname,
-    entry: './frontend/enter_here.jsx', //half_time is just an example
+    entry: './frontend/enter_here.jsx', 
     output: {
         path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
         filename: 'bundle.js'
