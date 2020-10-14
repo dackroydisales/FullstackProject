2e13fbba83f93958a5c65f8e1b14ef5392bdcc0b
Daniel Ackroyd_Isales <dackroydisales@gmail.com>
2020-10-14 10:25:00 -0700

    Finish auth (backend)

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,10 +1,15 @@
 class Api::SessionsController < ApplicationController
+
+    #test
+  skip_before_action :verify_authenticity_token
+
+
   def create 
     @user = User.find_by_credentials(
       params[:user][:username],
       params[:user][:password]
     )
-
+    # debugger
     if @user
       login(@user)
       render "api/users/show"
---
+++ app/models/user.rb
@@ -0,0 +1,51 @@
+class User < ApplicationRecord
+
+  attr_reader :password
+
+  validates :username, :email, :password_digest, :session_token, presence: true
+  validates :username, :email, uniqueness: true
+  validates :password, length: { minimum: 6 }, allow_nil: true
+
+  after_initialize :ensure_session_token
+
+  def self.find_by_credentials(username, password)
+    #database query
+    user = User.find_by(username: username)
+    
+
+    
+    return nil unless user
+    #implicit return
+    user.is_password?(password) ? user : nil
+end
+
+
+  def password=(password)
+    #for verification only
+    @password = password
+    #saved value
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def reset_session_token!
+    #first generate the session token
+    self.session_token = User.generate_session_token
+    #! = 'loud/fast error'
+    self.save!
+    #return
+    self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64
+  end
+
+end
