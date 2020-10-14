3e7424bdb0d7f66314d914eb15955eaa998e6225
Daniel Ackroyd_Isales <dackroydisales@gmail.com>
2020-10-14 11:40:29 -0700

    Finish front-end user authentication (actions, reducers, store, util)

--- frontend/enter_here.jsx
+++ frontend/enter_here.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+// import { signup, login, logout } from './util/session_api_util'
+import configureStore from './store/store';
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
+  const store = configureStore();
   ReactDOM.render(<h1>Aqualung</h1>, root);
+
 });
